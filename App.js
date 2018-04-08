94f5dee81ee2c5c92a51c32bf04e329268620a8b
Laura Cárdenas <laura.cardenas@axiacore.com>
2018-04-08 13:05:13 -0500

    :bug: category room added

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
           <Container>
             <Header />
               <Content>
-                <RoomsCreateRoom /> 
+                <RoomsCreateRoom />
                 <RoomsList />
               </Content>
           </Container>
