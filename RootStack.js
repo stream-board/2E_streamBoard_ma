e63fb42c0b9d520537bcd8f7fc97c31ed30332ff
jscontreraspUN <jscontrerasp@unal.edu.co>
2018-04-09 21:38:41 -0500

    Fix Routing :hankey:

---
+++ RootStack.js
@@ -0,0 +1,22 @@
+import CreateRoomPage from './rooms/components/RoomsCreateRoom.js';
+import RoomsDetail from './rooms/components/RoomsDetail.js';
+import LoginPage from './routes/Login/Login.js';
+import MainMenuPage from './routes/MainMenu/MainMenu.js';
+import JoinRoomPage from './routes/MainMenu/JoinRoom.js';
+
+import { StackNavigator } from "react-navigation";
+
+
+export const RootStack = StackNavigator(
+  {
+    Login: { screen: LoginPage },
+    MainMenu: { screen: MainMenuPage },
+    JoinRoom: { screen: JoinRoomPage },
+    CreateRoom: { screen: CreateRoomPage },
+    RoomsDetail: { screen: RoomsDetail},
+  },
+  {
+    initialRouteName: "Login",
+  }
+);
+
