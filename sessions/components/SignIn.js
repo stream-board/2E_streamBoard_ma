6b468aed34e6a9039e861de1eda0980c6352ed2b
Nicolas Maldonado <nlmaldonadog@unal.edu.co>
2018-04-09 23:48:31 -0500

    :hankey: :sparkles: Flujo desde log in

--- sessions/components/SignIn.js
+++ sessions/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View} from 'react-native';
 import { SessionCreateMutation } from './../TypesDef'
 import { Mutation } from 'react-apollo';
@@ -22,62 +22,66 @@ const mapStateToProps = (state) => ({
   queryParams: state.queryParams,
 })
 
-export const SignIn = () => {
-    onForm = (createSession) => {
-      return (
-        <Container>
-          <Title style={styles.titleElement}>HLA</Title>
+export class SignIn extends Component {
+  constructor(props) {
+    super(props);
 
-          <Item style={styles.formElement}>
-            <Label>Email</Label>
-            <Input
-            onChangeText= {(text)=>{
-              Store.dispatch( sessionActionCreators.addUserEmail(text));
-            }}
-            />
-          </Item>
+    this.state = { open: false, text: '' };
+    this.onForm = this.onForm.bind(this);
+    this.onCreateSession = this.onCreateSession.bind(this);
+  }
 
-          <Item style={styles.formElement}>
+  onForm(createSession) {
+    return (
+      <Container>
+        <Title style={styles.titleElement}>HLA</Title>
+
+        <Item style={styles.formElement}>
+          <Label>Email</Label>
           <Input
-            placeholder='Password'
-            onChangeText= {(text)=>{
-              Store.dispatch( sessionActionCreators.addUserPassword(text));
-            }}
+          onChangeText= {(text)=>{
+            Store.dispatch( sessionActionCreators.addUserEmail(text));
+          }}
           />
-          </Item>
+        </Item>
 
-          <Button style={styles.buttonStyle} rounded success
-            onPress={() => {
-              createSession({ 
-                variables: { 
-                  session: Store.getState().sessionCreateParams 
-                }
-              })
-            }}
-          ><Text>INGRESAR</Text></Button>
-        </Container>
-      )
-    };
+        <Item style={styles.formElement}>
+        <Input
+          placeholder='Password'
+          onChangeText= {(text)=>{
+            Store.dispatch( sessionActionCreators.addUserPassword(text));
+          }}
+        />
+        </Item>
 
-    onCreateSession = (data) => {
-      console.log(data);
-      Store.dispatch(sessionActionCreators.addCurrentUser(data.createSession));
-      //console.log(this.props.navigation.navigate('MainMenu'))
-      return (
-        <View>
-          <Text>{Store.getState().currentUser.name}</Text>
-        </View>
-      )
-    };
+        <Button style={styles.buttonStyle} rounded success
+          onPress={() => {
+            createSession({ 
+              variables: { 
+                session: Store.getState().sessionCreateParams 
+              }
+            })
+          }}
+        ><Text>INGRESAR</Text></Button>
+      </Container>
+    )
+  };
 
+  onCreateSession(data){
+    console.log(data);
+    console.log(this.props.navigation);
+    Store.dispatch(sessionActionCreators.addCurrentUser(data.createSession));
+    return this.props.navigation.navigate('MainMenu')
+  };
 
+  render(){
     return (
       <Mutation 
         mutation={SessionCreateMutation} 
       >
         {(createSession, { loading, error, data }) => (
           <View>
-          {(data ? onCreateSession(data) : onForm(createSession))}  
+          {(data ? this.onCreateSession(data) : this.onForm(createSession))}  
           {loading && <Spinner />}
           {error && <Text> Error: ${error}</Text>}  
           </View>
@@ -85,6 +89,7 @@ export const SignIn = () => {
       </Mutation>
     )
   }
+}
 
 export default connect(mapStateToProps)(SignIn)
 
