9d321e214c43fd43a835a67420f48f493acbd732
Juan Rubio <jcrubioa@unal.edu.co>
2018-05-02 23:27:47 -0500

    stuff

--- sessions/Server.js
+++ sessions/Server.js
@@ -1,4 +1,4 @@
-export const serverIp = 'ec2-35-168-17-78.compute-1.amazonaws.com';
-export const port = '4003';
+export const serverIp = '35.190.138.158';
+export const port = '80';
 export const entryPoint = '';
 
